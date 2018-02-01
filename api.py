cd3ebb469ea9840e63dd0fb5a9c0223227a9d596
Brandon Dossantos <leix@Brandons-MacBook-Pro.local>
2018-01-31 23:54:35 -0500

    Changed port from 5000 to 6000

--- api.py
+++ api.py
@@ -246,6 +246,6 @@ if __name__ == '__main__':
     if not os.path.exists('teams.db'): 
         open('teams.db', 'w').close() 
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=6000)
 
             
